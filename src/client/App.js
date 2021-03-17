0a93f3932b097d21087deeb9927a2b4b69e25dc6
Azim Islam <ec19205@qmul.ac.uk>
2021-03-17 20:48:14 +0000

    Removed unnecessary lifecyle hook

--- src/client/App.js
+++ src/client/App.js
@@ -7,12 +7,6 @@ import RegisterBox from './components/RegisterBox';
 export default class App extends Component {
   state = { username: null };
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
   render() {
     const { username } = this.state;
     return (
