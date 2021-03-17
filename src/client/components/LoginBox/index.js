df540a8ec71d80d7b053d15fdeab27438720a850
LarbiErnst <larbimernst@gmail.com>
2021-03-17 13:35:29 +0000

    added clientside login input handling

--- src/client/components/LoginBox/index.js
+++ src/client/components/LoginBox/index.js
@@ -5,15 +5,39 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import './style.css';
+//import dataStreamHandler
 
 export default class LoginBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: ' ',
+            password: ' '
+            }
+        }
+    
     render() {
         return (
             <form id="login-box">
-                <TextField id="outlined-basic" label="E-mail" type="email" variant="outlined" />
-                <TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
-                <Button variant="contained" color="primary">Login</Button>
+                <TextField id="outlined-basic" label="E-mail" type="email" variant="outlined"
+                onChange = {(event) => this.handleInput("email", event.target.value)}/><br/>
+                <TextField id="outlined-basic" label="Password" type="password" variant="outlined"
+                onChange = {(event) => this.handleInput("password", event.target.value)}/><br/>
+                <Button variant="contained" color="primary" onClick = {(event) => this.submitData(event)} >Login </Button>
+                
             </form>
         )
     }
+    handleInput(name ,value){ //use an interface to enforce implementation of these methods for LoginBox and RegisterBox -> Proxy Design Pattern 
+        var parameter = {label: name, val: value}  
+        this.setState(parameter)  
+    }
+
+    submitData(event){
+        alert(this.state.email + "," + this.state.password)
+    }
+    setState(property){
+        this.state[property.label] = property.val
+    }
+
 }
