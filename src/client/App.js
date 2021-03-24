a356cdc0309a6894164122eba95e6cee029af293
LarbiErnst <larbimernst@gmail.com>
2021-03-24 16:57:46 +0000

    updated App.js to account for listing

--- src/client/App.js
+++ src/client/App.js
@@ -4,13 +4,20 @@ import './app.css';
 import ReactImage from './react.png';
 import LoginBox from './components/LoginBox';
 import RegisterBox from './components/RegisterBox';
+
+import Listings from './components/ListingsBox';
+import ListingsBox from './components/ListingsBox';
+import FrontPage from './components/FrontPage';
+
 import ApiHandler from './helpers/ApiHandler';
+
 export default class App extends Component {
   constructor(props){
     super(props)
     this.toggleregister = this.toggleregister.bind(this);
     this.state = { username: null, 
       isregistered: true,
+      isloggedin: true,
       text: "Create an account"
     };
     this.ApiHandler = new ApiHandler();
@@ -36,12 +43,21 @@ export default class App extends Component {
     const { username } = this.state;
     
     
+    //need an event for when logged in -> for sake of testing I've set isloggedin to true
+
     return (
       
+      //need to take logged in state from inside LoginBox to out here, use props??
+      //need switch case for when logged in??
+      
+      //have to find a way of doing this in a more OOP way
       <div>
         
-        {(this.state.isregistered) ?  <LoginBox></LoginBox> :  <RegisterBox></RegisterBox>}
-        <Button onClick= {this.toggleregister} variant="contained" color="primary">{this.state.text}</Button>
+        
+        {(this.isloggedin) ?  <ListingsBox></ListingsBox>:  
+        //transitions from front page into ListingBox 
+        //-> replace with <FrontPage> </FrontPage> later and use state with switch to access other pages or find an more 'OOP way'
+            (this.state.isregistered) ? <LoginBox></LoginBox> :  <RegisterBox></RegisterBox>, <Button onClick= {this.toggleregister} variant="contained" color="primary">{this.state.text}</Button>}
       </div>
     );
   }
