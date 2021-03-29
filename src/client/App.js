9f4cc3c439ed9c703ba70e1bbdd5deec17b19cf5
Azim Islam <ec19205@qmul.ac.uk>
2021-03-29 20:09:13 +0100

    Added Routing

--- src/client/App.js
+++ src/client/App.js
@@ -1,49 +1,17 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import './app.css';
-import Typography from '@material-ui/core/Typography';
-import LoginBox from './components/LoginBox';
-import RegisterBox from './components/RegisterBox';
-import Header from './components/Header';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import ApiHandler from './helpers/ApiHandler';
-export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.toggleregister = this.toggleregister.bind(this);
-    this.state = { username: null, 
-      isregistered: true,
-      text: "Create an account",
-      text2: "Don't have an account?"
-    };
-    this.ApiHandler = new ApiHandler();
-  }
 
- toggleregister(){
-   this.setState({isregistered: !this.state.isregistered})
-   if (this.state.isregistered){
-     this.setState({text: "Login", text2: "Already have an account?"})
-   } else {
-    this.setState({text: "Create an account", text2: "Don't have an account?"})
-   }
+import Home from './routes/Home'
 
- }
-
+export default class App extends React.Component {
   render() {
-    const { username } = this.state;
-    
-    
     return (
-      <div>
-        <Header/>
-        {(this.state.isregistered) ?  <LoginBox></LoginBox> :  <RegisterBox></RegisterBox>}
-        {
-          <div id="registerBox">
-            <Typography id="text">{this.state.text2}</Typography>
-            <Button onClick= {this.toggleregister} variant="contained" color="primary">{this.state.text}</Button>
-          </div>
-        }
-      </div>
-    );
+      <Router>
+        <Route path="/" >
+          <Home/>
+        </Route>
+      </Router>
+    )
   }
 }
---
+++ src/client/routes/Home/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './style.css';
+import Typography from '@material-ui/core/Typography';
+import LoginBox from '../../components/LoginBox';
+import RegisterBox from '../../components/RegisterBox';
+import Header from '../../components/Header';
+import ApiHandler from '../../helpers/ApiHandler';
+
+export default class Home extends Component {
+  constructor(props){
+    super(props)
+    this.toggleregister = this.toggleregister.bind(this);
+    this.state = { username: null, 
+      isregistered: true,
+      text: "Create an account",
+      text2: "Don't have an account?"
+    };
+    this.ApiHandler = new ApiHandler();
+  }
+
+ toggleregister(){
+   this.setState({isregistered: !this.state.isregistered})
+   if (this.state.isregistered){
+     this.setState({text: "Login", text2: "Already have an account?"})
+   } else {
+    this.setState({text: "Create an account", text2: "Don't have an account?"})
+   }
+
+ }
+
+  render() {
+    const { username } = this.state;
+    
+    
+    return (
+      <div>
+        <Header/>
+        {(this.state.isregistered) ?  <LoginBox></LoginBox> :  <RegisterBox></RegisterBox>}
+        {
+          <div id="registerBox">
+            <Typography id="text">{this.state.text2}</Typography>
+            <Button onClick= {this.toggleregister} variant="contained" color="primary">{this.state.text}</Button>
+          </div>
+        }
+      </div>
+    );
+  }
+}
