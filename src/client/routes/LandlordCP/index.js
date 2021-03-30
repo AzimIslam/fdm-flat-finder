21c6373c06a863f4d27637a9b488f680d082c8c1
Azim Islam <ec19205@qmul.ac.uk>
2021-03-30 23:08:00 +0100

    Added toolbar

--- src/client/routes/LandlordCP/index.js
+++ src/client/routes/LandlordCP/index.js
@@ -1,8 +1,11 @@
 import React, {Component} from "react";
-import './style.css'
+import './style.css';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/typography";
+import 'fontsource-roboto';
 
-
-export default class LandlordCP extends React.Component {
+export default class Landlord extends React.Component {
     constructor() {
         super()
         this.state = {
@@ -18,7 +21,14 @@ export default class LandlordCP extends React.Component {
                 {
                     this.state.userType != 'landlord'? <p>You are not a landlord</p>:
                     this.state.loggedIn != 'true' ? <p>You are not logged in</p>:
-                    <p>Test</p>
+                    <div>
+                        <Toolbar style={{backgroundColor: "#3f51b5"}} variant="dense">
+                            <Typography style={{fontWeight: "lighter", color: "white"}}variant="h6">
+                                Landlord Control Panel
+                            </Typography>
+                            <Button style={{color: 'white'}}>Login</Button>
+                        </Toolbar>
+                    </div>
                 }
             </div>
         )
