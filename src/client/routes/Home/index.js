2c10867c5006d35100c15726b270e5a859e19c88
Azim Islam <ec19205@qmul.ac.uk>
2021-03-30 23:38:32 +0100

    Fixed toolbar

--- src/client/routes/Home/index.js
+++ src/client/routes/Home/index.js
@@ -32,7 +32,7 @@ export default class Home extends Component {
   render() {
     const { username } = this.state;
     return (
-      <div>
+      <div id="homeBack">
         <Header/>
         {(this.state.isregistered) ?  <LoginBox></LoginBox> :  <RegisterBox></RegisterBox>}
         {
--- src/client/routes/LandlordCP/index.js
+++ src/client/routes/LandlordCP/index.js
@@ -1,8 +1,12 @@
 import React, {Component} from "react";
 import './style.css';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
 import Typography from "@material-ui/core/typography";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import 'fontsource-roboto';
 
 export default class Landlord extends React.Component {
@@ -22,12 +26,25 @@ export default class Landlord extends React.Component {
                     this.state.userType != 'landlord'? <p>You are not a landlord</p>:
                     this.state.loggedIn != 'true' ? <p>You are not logged in</p>:
                     <div>
-                        <Toolbar style={{backgroundColor: "#3f51b5"}} variant="dense">
-                            <Typography style={{fontWeight: "lighter", color: "white"}}variant="h6">
-                                Landlord Control Panel
-                            </Typography>
-                            <Button style={{color: 'white'}}>Login</Button>
-                        </Toolbar>
+                    <Toolbar style={{backgroundColor: "#3f51b5"}} variant="dense">
+                        <IconButton style={{color: "white"}} edge="start" color="inherit" aria-label="open drawer">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography style={{fontWeight: "lighter", color: "white"}}variant="h6">
+                            Landlord Control Panel
+                        </Typography>
+
+                        <div class="userTools">
+                            <IconButton style={{color: "white"}} color="inherit">
+                                <Badge badgeContent={103} color="secondary">
+                                    <MailIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton style={{color: "white"}} start="end" color="inherit">
+                                <AccountCircle />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
                     </div>
                 }
             </div>
