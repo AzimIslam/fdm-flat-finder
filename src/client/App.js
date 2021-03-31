8684ae08c751946c1dff179501bbc3957612ceb9
Azim Islam <ec19205@qmul.ac.uk>
2021-03-31 17:44:00 +0100

    Merging with Mahdi branch

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import RegisterBox from './components/RegisterBox';
-import LandlordCP from './routes/LandlordCP'
+import LandlordCP from './routes/LandlordCP';
 import Home from './routes/Home'
+
 
 export default class App extends React.Component {
   render() {
@@ -11,7 +12,6 @@ export default class App extends React.Component {
         <Switch>
           <Route path="/landlord"  component={LandlordCP} />
           <Route path="/"  component={Home} />
-  
         </Switch>
       </Router>
     )
