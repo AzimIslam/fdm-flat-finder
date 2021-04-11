fb641ac06a3ac9ae9b1413a19932741c1c971530
sevenspace <s.pang@se19.qmul.ac.uk>
2021-04-11 17:58:43 +0100

    scroll bar?

--- src/client/components/AccountSettings/index.js
+++ src/client/components/AccountSettings/index.js
@@ -3,13 +3,12 @@ import TextField from "@material-ui/core/TextField";
 import './style.css';
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper'
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Draggable from 'react-draggable';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
 
 export default class AddCreateTicketForm extends React.Component {
     constructor(props) {
@@ -18,8 +17,11 @@ export default class AddCreateTicketForm extends React.Component {
             //text box with email can't edit 
             //enter password, new password
             // first name, last name, agency name
-
-            details: [],
+            agencyName: '',
+            firstName: '',
+            lastName: '',
+            currentPassword: '',
+            email: '',
             newPassword: '',
             userID: sessionStorage.getItem('user_id'),
         }
@@ -49,57 +51,42 @@ export default class AddCreateTicketForm extends React.Component {
         })
     }
 
-    async componentDidMount() {
-        await fetch(`/api/user/getUserDetails`)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({details: data})
-            });
 
-        console.log(this.state.details)
+    async componentDidMount() {
+        fetch('/api/user/getUserDetails', {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+           //* body: JSON.stringify({ListingID: id}) *//
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.setState({agencyName: data['AgencyName'], firstName: data['FirstName'], lastName: data['LastName'], currentPassword: data['Password'], email: data['Email'], modalOpen: true})
+        })
+
     }
 
 
     render() {
 
         return( 
-        <TableContainer component={Paper}>
-        <Table id="table" aria-label="simple table">
-        <TableHead>
-            <TableRow>
-            <TableCell align="left">Agency Name</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell align="left">First Name</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell align="left">Last Name</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell align="left">Email</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell align="left">Password</TableCell>
-            </TableRow>   
-        </TableHead>
-        <TableBody>
-            {this.state.details.map((row) => (
-            <TableRow key={row.UserID}>
-                <TableCell align="left">{row.agencyName}</TableCell>
-                <TableCell align="left">{row.firstName}</TableCell>
-                <TableCell align="left">{row.lastName}</TableCell>
-                <TableCell align="left">{row.email}</TableCell>
-                <TableCell align="left">{row.currentPassword}</TableCell>
-                <TableCell align="left" style={{overflow: 'none'}}>
-                    <div className="buttons">
-                        <Button onClick={() => this.editRequest()} style={{marginLeft: "10px"}} variant="contained" color="primary">Edit</Button>
-                    </div>
-                </TableCell>
-            </TableRow>
-            ))}
-        </TableBody>
-        </Table>
-    </TableContainer>
+            <div>
+            <DialogContent>
+                <div id="editForm">
+                <TextField value={this.state.agencyName} id="standard-basic" onChange={(e) => this.setState({agencyName: e.target.value})} label="Agency Name"></TextField>
+                <TextField value={this.state.firstName} id="standard-basic" onChange={(e) => this.setState({firstName: e.target.value})} label="First Name"></TextField>
+                <TextField value={this.state.lastName} id="standard-basic" onChange={(e) => this.setState({lastName: e.target.value})} label="Last Name"></TextField>
+                <TextField disabled={true} value={this.state.email} id="standard-basic" onChange={(e) => this.setState({email: e.target.value})} label="Email"></TextField>
+                <TextField value={this.state.currentPassword} id="standard-basic" onChange={(e) => this.setState({currentPassword: e.target.value})} label="Current Password"></TextField>
+                <TextField value={this.state.newPassword} id="standard-basic" onChange={(e) => this.setState({newPassword: e.target.value})} label="New Password"></TextField>
+                </div>
+            </DialogContent>
+
+             <DialogActions>
+             <Button onClick={() => this.editRequest()} style={{marginLeft: "10px"}} variant="contained" color="primary">Edit</Button>
+            </DialogActions>
+         </div>
         )
       
     }
